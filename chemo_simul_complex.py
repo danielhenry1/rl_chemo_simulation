ab0592a35021e0311aa42bcdf5b7a1a9ca7e9462
Daniel Henry <danielhenry@DN0a1f0880.SUNet>
2019-12-12 00:07:45 -0800

    tuned params clean code

--- chemo_simul_complex.py
+++ chemo_simul_complex.py
@@ -2,6 +2,7 @@ import util, math, random
 from collections import defaultdict
 from util import ValueIteration
 import numpy as np
+import random
 
 
 # This chemotherapy model is based on "Reinforcement learning-based control of drug dosing for cancer chemotherapy treatment"
@@ -29,14 +30,14 @@ r1 = Per unit growth rate of tumor cells
 r2 = Per unit growth rate of normal cells
 s = Immune cell influx rate
 alpha = Immune cell threshold rate
-row = Immune repsonse rate
+rho = Immune repsonse rate
 
 '''
-
 class ChemoMDPComplex(util.MDP):
+
     def __init__(self, n_cells_init, t_cells_init, i_cells_init, \
-        a1=0.2, a2=0.3, a3=0.1, b1=1, b2=1, c1=1, c2=0.5, c3=1, c4=1, \
-        d1=0.2, d2=1, r1=1.5, r2=1, s=0.33, alpha=0.3, row=0.01):
+        a1=0.32, a2=0.38, a3=0.10, b1=0.2, b2=0.2, c1=0.5, c2=0.4, c3=0.585, c4=0.6, \
+        d1=0.25, d2=1, r1=1.4, r2=1, s=0.37, alpha=0.35, rho=0.01, max_months = 7):
 
         self.n_cells_init = n_cells_init
         self.t_cells_init = t_cells_init
@@ -56,116 +57,116 @@ class ChemoMDPComplex(util.MDP):
         self.r2 = r2
         self.s = s
         self.alpha = alpha
-        self.row = row
+        self.rho = rho
+        self.max_months = max_months
+
+
+
+        print("a1: " + str(self.a1))
+        print("a2: " + str(self.a2))
+        print("a3: " + str(self.a3))
+        print("b1: " + str(self.b1))
+        print("b2: " + str(self.b2))
+        print("c1: " + str(self.c1))
+        print("c2: " + str(self.c2))
+        print("c3: " + str(self.c3))
+        print("c4: " + str(self.c4))
+        print("d1: " + str(self.d1))
+        print("d2: " + str(self.d2))
+        print("r1: " + str(self.r1))
+        print("r2: " + str(self.r2))
+        print("s: " + str(self.s))
+        print("alpha: " + str(self.alpha))
+        print("rho: " + str(self.rho))
+
 
 
     # Return the start state.
     def startState(self):
-        #(num of normal cells, num of tumor cells, num of immune cells, drug concentration)
-        return (self.n_cells_init, self.t_cells_init, self.i_cells_init, 0)
+        #(num of normal cells, num of tumor cells, num of immune cells, drug concentration, max_months)
+        return (self.n_cells_init, self.t_cells_init, self.i_cells_init, 0, 0)
 
-    # Return set of actions possible from |state|.
-    # All logic for dealing with end states should be placed into the succAndProbReward function below.
+    # Return set of actions
     def actions(self, state):
         return (0, 0.01, 0.02, 0.03, 0.04, 0.06, 0.08, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)
-
-    # Given a |state| and |action|, return a list of (newState, prob, reward) tuples
-    # corresponding to the states reachable from |state| when taking |action|.
-    # A few reminders:
-    # * Indicate a terminal state (after quitting, busting, or running out of cards)
-    #   by setting the deck to None.
-    # * If |state| is an end state, you should return an empty list [].
-    # * When the probability is 0 for a transition to a particular new state,
-    #   don't include that state in the list returned by succAndProbReward.
 
 
     def succAndProbReward(self, state, action):
         # BEGIN_YOUR_CODE 
 
-        n_cells, t_cells, i_cells, d_c = state
+        n_cells, t_cells, i_cells, d_c, t = state
 
-        #print("ACTIONS: " + str(action))
-        #print("t_cells: " + str(t_cells))
-        # Terminal state
+        #end state
         if n_cells is None and t_cells is None and i_cells is None: return []
 
-        # # cured!
-        # if M <= 0: return [((None, None, t), 1, self.curedReward)]
-
-        #CALCULATE REWARD LATER
-
-        end_treatment_reward = 10
-        if t_cells < 0.05: return [((None, None, None, 0), 1, end_treatment_reward)]
+        #max months reached
+        end_treatment_reward = 3 * t_cells
+        if t == self.max_months: return [((None, None, None, None, t), 1, end_treatment_reward)]
         
         results = []
 
         #New number of normal cells
-        new_n_cells = self.r2 * n_cells * (1 - self.b2 * n_cells) - self.c4 * n_cells * t_cells - self.a3 * n_cells * d_c 
+        delta_n_cells = self.r2 * n_cells * (1 - self.b2 * n_cells) - self.c4 * n_cells * t_cells - self.a3 * n_cells * d_c 
 
         #New number of tumor cells
-        new_t_cells = self.r1 * t_cells * (1 - self.b1 * t_cells) - self.c2 * i_cells * t_cells - self.c3 * t_cells * n_cells \
+        delta_t_cells = self.r1 * t_cells * (1 - self.b1 * t_cells) - self.c2 * i_cells * t_cells - self.c3 * t_cells * n_cells \
         - self.a2 * t_cells * d_c
 
         #New number of immune cells
-        new_i_cells = self.s + ((self.row * i_cells * t_cells) / (self.alpha + t_cells)) - self.c1 * i_cells * t_cells - self.d1 * i_cells \
+        delta_i_cells = self.s + ((self.rho * i_cells * t_cells) / (self.alpha + t_cells)) - self.c1 * i_cells * t_cells - self.d1 * i_cells \
         - self.a1 * i_cells * d_c
 
         #New drug concentration
-        new_d_c = - self.d2 * d_c + action
+        delta_d_c = - self.d2 * d_c + action
 
-        #next state
-        nextState = (new_n_cells, new_t_cells, new_i_cells, new_d_c)
-
-        #Reward Function based on paper
-        currReward = 0
-        if new_t_cells < t_cells:
-            currReward = (t_cells - new_t_cells) / (t_cells)
-
-        #Living State
-        if t_cells > 1:
-            newProbLiving = 0
-        else:
-            newProbLiving = t_cells / 1
-        results.append((nextState, newProbLiving, currReward))
-
-        #Death State
-        deathState = (None, None, None, 0)
-        results.append((deathState, 1 - newProbLiving, currReward))
 
         # cured!
+        if (delta_t_cells + t_cells) <= 0: return [((None, None, None, None, t), 1, 5)]
+
+        #next state
+        nextState = (delta_n_cells + n_cells, delta_t_cells + t_cells, delta_i_cells + i_cells, delta_d_c + d_c, t + 1)
 
 
-        if new_t_cells <= 0: return [((None, None, None, 0), 1, 20)]
+        #Reward based on proportional t-cell decrease
+        currReward = 0
+        if (delta_t_cells) < t_cells:
+            currReward = (t_cells - (delta_t_cells)) / (t_cells)
+        
+        
+        #hazard function
+        newProbLiving = (i_cells + delta_i_cells) / (delta_t_cells  + t_cells)
 
+        results.append((nextState,  min(newProbLiving, 1), currReward))
+
+        #Death State
+        deathState = (None, None, None, None, t+1)
+        results.append((deathState, 1 -  min(newProbLiving, 1), -5))
 
         return results
-
-
-
 
         # END_YOUR_CODE
 
     def discount(self):
         return 1
 
-# Return a single-element list containing a binary (indicator) feature
-# for the existence of the (state, action) pair.  Provides no generalization.
-def ChemoComplexFeatureExtractor(state, action):
-    n_cells, t_cells, i_cells, d_c = state
-    features = []
-    if n_cells is not None:
-        n_bucket = n_cells * 10 // 1
-        features.append(("normal cells" + str(n_bucket) + str(action),1))
-    if t_cells is not None:
-        t_bucket = t_cells * 10 // 1
-        features.append(("tumor cells" + str(t_bucket) + str(action),1))
-    if i_cells is not None:
-        i_bucket = i_cells * 10 // 1
-        features.append(("immune cells" + str(i_bucket) + str(action),1))
-    if n_cells is not None and t_cells is not None and i_cells is not None:
-        features.append(("normal cells" + str(n_bucket) + "tumor cells" + str(t_bucket) + "immune cells" + str(i_bucket) + str(action),1))
-    return features
 
+def ChemoComplexFeatureExtractorWrapper(k):
+    def ChemoComplexFeatureExtractor(state, action):
+        n_cells, t_cells, i_cells, d_c, t = state
+        features = []
+        if n_cells is not None:
+            n_bucket = math.floor(n_cells * k)
+            features.append(("normal cells" + str(n_bucket) + str(action),1))
+        if t_cells is not None:
+            t_bucket = math.floor(t_cells * k)
+            features.append(("tumor cells" + str(t_bucket) + str(action),1))
+        if i_cells is not None:
+            i_bucket = math.floor(i_cells * k)
+            features.append(("immune cells" + str(i_bucket) + str(action),1))
+        if n_cells is not None and t_cells is not None and i_cells is not None:
+            features.append(("normal cells" + str(n_bucket) + "tumor cells" + str(t_bucket) + "immune cells" + str(i_bucket) + str(action),1))
+        return features
+    return ChemoComplexFeatureExtractor
     
 
 ############################################################
